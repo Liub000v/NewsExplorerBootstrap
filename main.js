390379ec30fcb50ea6a81eb905133ac5f9daf176
Liub000v <liubov97@mail.ru>
2021-11-22 19:49:03 +0300

    Changed port

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const server = express()
-const port = 3001
 
 server.set('port', process.env.PORT || 3001);
 
 server.use(express.static(path.join(__dirname, '/')));
 
-server.listen(port, function () {
-  console.log('listening on port: ', port);
+server.listen(server.get('port'), function () {
+  console.log('listening on port: ', server.get('port'));
 });
